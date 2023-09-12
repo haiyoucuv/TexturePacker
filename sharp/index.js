9d46ff5dda1bd957cfa8423ebb2a6933178e5612
邱旭 <qiuxu@duiba.com.cn>
2023-09-12 19:12:09 +0800

    1.1.0

---
+++ sharp/index.js
@@ -0,0 +1,8 @@
+
+// if (process.arch.includes('x64')) {
+//     module.exports = require('./build/sharp_x64');
+// } else {
+//     module.exports = require('./build/sharp_arm');
+// }
+
+module.exports = require('./lib');
