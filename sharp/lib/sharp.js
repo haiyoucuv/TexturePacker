6573a5b15aa7bf589283ddc43147964112aa5e65
邱旭 <qiuxu@duiba.com.cn>
2023-09-13 11:38:13 +0800

    1.1.0

--- sharp/lib/sharp.js
+++ sharp/lib/sharp.js
@@ -1,5 +1,12 @@
-if (process.arch.includes('x64')) {
-	module.exports = require('../build/sharp_x64/build/Release/sharp.node');
+const Os = require('os');
+const platform = Os.platform();
+
+if (platform === 'win32') {
+	module.exports = require('../build/sharp_win32/build/Release/sharp.node');
 } else {
-	module.exports = require('../build/sharp_arm/build/Release/sharp.node');
+	if (process.arch.includes('x64')) {
+		module.exports = require('../build/sharp_x64/build/Release/sharp.node');
+	} else {
+		module.exports = require('../build/sharp_arm/build/Release/sharp.node');
+	}
 }
